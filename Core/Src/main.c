27733afc54cc205d4337ce29277b8a6fd44cfbb2
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-08-20 06:26:50 +0530

    Empty project added

---
+++ Core/Src/main.c
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+// Macro defines
+#define TURN_ON_LED()            turn_led_on(TURN_ON)
+#define TURN_OFF_LED()           turn_led_on(TURN_OFF)
+#define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
+
+/**
+ * Enumerations
+ */
+typedef enum {
+    TURN_OFF,
+    TURN_ON,
+    TURN_TOGGLE
+} LedState_t;
+
+/**
+ * @brief Trun the built in LED on/off/toggle
+ *
+ * @param state - TURN_ON / TURN_OFF / TURN_TOGGLE
+ */
+void turn_led_on(LedState_t state) {
+    if(state == TURN_TOGGLE){
+        GPIOC->ODR ^= GPIO_ODR_ODR13;
+    } else if(state == TURN_ON) {
+        GPIOC->ODR &= ~(GPIO_ODR_ODR13);
+    } else {
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+    }
+}
+
+/**
+ * @brief Configure the system clock as 8MHz using
+ * external crystal oscillator.
+ */
+void config_sys_clock() {
+    // Enable HSE (High-Speed External) oscillator
+    RCC->CR |= RCC_CR_HSEON;
+    while ((RCC->CR & RCC_CR_HSERDY) == 0);  // Wait for HSE to be ready
+
+    // Select HSE as the system clock source
+    RCC->CFGR &= ~RCC_CFGR_SW;  // Clear SW bits
+    RCC->CFGR |= RCC_CFGR_SW_HSE;  // Set SW bits to select HSE as system clock
+
+    // Wait until HSE is used as the system clock source
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);
+}
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    config_sys_clock();
+
+    // Enable clock for GPIOC peripheral
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    // Configure GPIO pin as output
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);  // Clear configuration
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;  // Set pin mode to general purpose output (max speed 10 MHz)
+
+    while(1) {
+        // Wait until the overflow flag is set.
+        while( (TIM1->SR & TIM_SR_UIF) == 0) {}
+        TIM1->SR &= ~(TIM_SR_UIF);
+        TOGGLE_LED();
+    }
+}
