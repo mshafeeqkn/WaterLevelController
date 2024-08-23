4746e5d67ed3f5625a829b7476c1ea12696d16cc
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-08-23 07:14:40 +0530

    WLC-11 Added command handling for RTC set time

--- Core/Src/config_mgr.c
+++ Core/Src/config_mgr.c
@@ -19,11 +19,25 @@
 /* Includes ------------------------------------------------------------------*/
 #include "config_mgr.h"
 #include "i2c.h"
+#include "rtc.h"
 #include "common.h"
 
+#define  SET_RTC_TIME       0x10
+
 static void on_new_command(uint8_t *data, uint8_t len) {
+    uint32_t i2c_data;
+    switch(data[0]) {
+        case SET_RTC_TIME:
+            i2c_data = (data[1] | data[2] << 8 | data[3] << 16 | data[4] << 24);
+            set_rtc_time(i2c_data);
+            break;
+    }
+}
+
+static void on_rtc_alarm() {
 }
 
 void init_config_mgr() {
+    init_rtc(on_rtc_alarm);
     i2c_slave_init(I2C_SLAVE_ADDRESS, on_new_command);
 }
