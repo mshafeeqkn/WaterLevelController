b58e856022fa0bdf0890dbc6e50ca25ca6e48a81
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-09-23 14:41:24 +0530

    Added comments in common.h

--- Core/Inc/common.h
+++ Core/Inc/common.h
@@ -28,32 +28,55 @@ extern "C" {
 
 #include "uart.h"
 
+// Input water level detect pin
 #define     WATER_LEVEL_100_PIN       GPIOA,8
 #define     WATER_LEVEL_80_PIN        GPIOB,15
 #define     WATER_LEVEL_60_PIN        GPIOB,14
 #define     WATER_LEVEL_40_PIN        GPIOB,13
 #define     WATER_LEVEL_20_PIN        GPIOB,12
 
+// Input water inflow detect pin
 #define     WATER_INFLOW_PIN          GPIOB,11
 
+// Output indicator pin
 #define     LED_LEVEL_100_PIN         GPIOA,0
 #define     LED_LEVEL_80_PIN          GPIOA,1
 #define     LED_LEVEL_60_PIN          GPIOA,3
 #define     LED_LEVEL_40_PIN          GPIOA,4
 #define     LED_LEVEL_20_PIN          GPIOA,5
 
+// Output Pin to show the inflow of water
 #define     LED_DRY_RUN_PIN           GPIOA,6
 
+// Output pin to control motor
 #define     PUMP_CONTROL_PIN          GPIOB,10
+
+// Input pin to trigger oneshot run
 #define     SINGLE_SHOT_PUMP_PIN      GPIOB,0
+
+// Input pin to set daily run time
 #define     PUMP_ON_TIME_SET_PIN      GPIOB,1
+
+// Input pin to set auto or manual mode
 #define     AUTO_MANUAL_PIN           GPIOA,2
 
+// Analog input pin to monitor the line voltage
 #define     VOLTAGE_MONITOR_PIN       /*GPIOA,*/7
+
+#define     LOW_VOLTAGE_INDIC_PIN     GPIOA,11
 
 #define     I2C_SLAVE_ADDRESS         0x28
 
 #define     THRESOLD_VOLTAGE_LEVEL    200
+
+// I2C pins
+#define     I2C_SDA_PIN               PORTB,6
+#define     I2C_SCL_PIN               PORTB,7
+
+// Serial port pins
+#define     UART_RX_PIN               PORTA,10
+#define     UART_TX_PIN               PORTA,9
+
 
 typedef enum {
     TANK_LEVEL_0,
